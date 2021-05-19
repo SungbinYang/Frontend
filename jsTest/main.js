d7f5eed40d42632d4d680acded028e1e798ceaf1
Robert.Yang <ysb05222@naver.com>
2021-05-19 22:38:56 +0900

    DOM API, 메소드 체이닝 추가
    DOM API, 메소드 체이닝 추가

--- jsTest/main.js
+++ jsTest/main.js
@@ -82,3 +82,55 @@ console.log(a1, b1, c1);
 hi();
 world();
 console.log(hisName);
+
+// DOM API
+// HTML 요소(Element) 1개 검색/찾기
+let boxEl = document.querySelector('.box');
+console.log(boxEl);
+
+// HTML 요소에 적용할 수 있는 메소드
+// 인수(Argument)를 추가 가능
+// 1 - 이벤트(Event, 상황)
+// 2 - 핸들러(Handler, 실행할 함수)
+// 요소의 클래스 정보 객체 활용
+boxEl.addEventListener('click', function() {
+    console.log('Click~');
+    boxEl.classList.add('active');
+    console.log(boxEl.classList.contains('active'));
+    boxEl.classList.remove('active');
+    console.log(boxEl.classList.contains('active'));
+});
+
+// HTML 요소(Element) 모두 검색 및 찾기
+const boxEls = document.querySelectorAll('.box');
+console.log(boxEls);
+
+// 칮은 요소들 반복해서 함수 실행!
+// 익명 함수를 인수로 추가
+boxEls.forEach(function() {});
+
+// 첫번째 매개변수 : 반복 중인 요소
+// 두번째 매개변수 : 반복 중인 번호
+boxEls.forEach(function(boxEl, index) {});
+
+// 출력
+boxEls.forEach(function(boxEl, index) {
+    boxEl.classList.add(`order-${index + 1}`);
+    console.log(index, boxEl);
+});
+
+// Getter, 값을 얻는 용도
+console.log(boxEl.textContent);
+
+// Setter, 값을 지정하는 용도
+boxEl.textContent = 'Sungbin';
+console.log(boxEl.textContent);
+
+const q = 'Hello~';
+// split : 문자를 기준으로 쪼개서 배열로 변환
+// reverse : 배열을 뒤집기
+// join : 배열을 인수 기준으로 문자로 병합해 반환
+const w = q.split('').reverse().join(''); // 메소드 체이닝
+
+console.log(q);
+console.log(w);
